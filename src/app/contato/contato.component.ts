4c7f7e7fabd960a75ce92371a7830118dc5368b5
Daniel Tsuzuki <massao.tsuzuki@gmail..com>
2023-11-10 00:31:20 -0300

    Implementado validaçoes no formulario reativo

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contato',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContatoComponent implements OnInit {
 
-  constructor() { }
+  formContato = this.fb.group({
+    nome: ["", [
+      Validators.minLength(4),
+      Validators.required
+    ]],
+    assunto: ["", [
+      Validators.minLength(10),
+      Validators.required
+    ]],
+    telefone: ["", [
+      Validators.minLength(11),
+      Validators.required
+    ]],
+    email: ["", [
+      Validators.required,
+      Validators.email
+    ]],
+    mensagem: ["", [
+      Validators.minLength(20),
+      Validators.maxLength(300),
+      Validators.required
+    ]]
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  enviarFormulario(){
+    alert("A mensasgem foi enviada!");
+    this.formContato.reset()
   }
 
 }
